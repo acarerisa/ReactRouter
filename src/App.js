9bf804e090ca3feb93eeaaf6cb29dc1094b07bc9
acarerisa <acarerisa84@gmail.com>
2021-03-20 00:04:18 +0300

    güncellendi

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import NotFound from './Pages/NotFound';
+import Topics from './Pages/Topics';
 
 function App() {
   return (    
@@ -20,6 +21,9 @@ function App() {
            <li>
              <Link to="/contact">Contact</Link>
            </li>
+           <li>
+             <Link to="/topics">Topics</Link>
+           </li>
          </ul>
        </nav>
        <hr/>
@@ -27,6 +31,7 @@ function App() {
          <Route exact path="/" component={Home}/>
          <Route path="/about" component={About}/>
          <Route path="/contact" component={Contact}/>
+         <Route path="/topics" component={Topics}/>
          <Route component={NotFound}/>
        </Switch>
      </div>
