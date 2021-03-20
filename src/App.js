05780c9762c596558b6e64ce0ce825341b666698
acarerisa <acarerisa84@gmail.com>
2021-03-20 17:51:44 +0300

    switch konusu anlatıldı

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import NotFound from './Pages/NotFound';
+
 
 function App() {
   return (    
@@ -22,12 +24,16 @@ function App() {
           </ul>
         </nav>
         <hr/>
+        <Switch>
        <Route exact path='/' component={Home}/>
 
        <Route path = '/about' component={About}/>
 
-       <Route path = '/contact' component={Contact}/>
+       <Route  path = '/contact' component={Contact}/>  
+
+       <Route component={NotFound} />    
        
+       </Switch>
        
        </BrowserRouter>
 
